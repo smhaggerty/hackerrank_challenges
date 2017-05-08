b3ed7426095694bf41a2eb37d1b30d87d61fad8a
smhaggerty <shanehaggerty@umail.ucsb.edu>
2017-05-08 00:55:19 -0700

    moved strip() methods from body of the for loop to body of the list comprehension

--- python3/set_mutations.py
+++ python3/set_mutations.py
@@ -1,12 +1,12 @@
 # take in input
 _, a, n  = int(input()), set(input()), int(input())
-operation_and_set = [input() for i in range(2 * n)]
+operation_and_set = [input().split() for i in range(2 * n)]
 # remove whitespace from set and convert elements to integers
 a.remove(' ')
 a = set(map(int, a))
 # loop through operations and
 for i in range(0, n * 2, 2):
-    operation = operation_and_set[i].split()[0]
-    active_set = set(map(int, set(operation_and_set[i + 1].split())))
+    operation = operation_and_set[i][0]
+    active_set = set(map(int, set(operation_and_set[i + 1])))
     eval('a.' + operation + '(active_set)')
 print(sum(a))
