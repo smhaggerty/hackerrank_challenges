1f661fb9392fa9f119071495b88c11df2c71329e
smhaggerty <shanehaggerty@umail.ucsb.edu>
2017-05-09 19:26:18 -0700

    formatted comments

--- python3/the_captains_room.py
+++ python3/the_captains_room.py
@@ -1,7 +1,10 @@
 _, hotelPatronList = input(), input().split()
 roomNumbers = set()
 patrons = set()
-# iterate through the list of patron's room numbers, adding unseen room numbers to the set of room numbers for each group. Since there is only one captains room, at the end of the traversal the captains room number will be missing from the set of patrons.
+# iterate through the list of patron's room numbers, adding unseen room numbers
+# to the set of room numbers for each group. Since there is only one captains
+# room, at the end of the traversal the captains room number will be missing
+# from the set of patrons.
 for patron in hotelPatronList:
     if patron in roomNumbers:
         patrons.add(patron)
