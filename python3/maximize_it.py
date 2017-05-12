5dccbc7f17ab5c21b77eee18d7aab68a338a583d
smhaggerty <shanehaggerty@umail.ucsb.edu>
2017-05-12 16:00:58 -0700

    removed unneeded set function

--- python3/maximize_it.py
+++ python3/maximize_it.py
@@ -1,6 +1,5 @@
 from itertools import product
 k, m = map(int, input().split())
-# store lists as sets because ordering/repeats don't matter within each list
-lists = [set(map(int, input().split()[1:])) for i in range(k)]
+lists = [map(int, input().split()[1:]) for i in range(k)]
 # evaluate the equation for each group of x values then print the max
 print(max([sum(map(lambda x: x**2, combo)) % m for combo in product(*lists)]))
