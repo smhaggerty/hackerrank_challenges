ed95ddd79133b331f5ce5e7dc9985c43599d9bd3
smhaggerty <shanehaggerty@umail.ucsb.edu>
2017-06-06 11:20:29 -0700

    changed to hangle cases lacking third input parameter

--- python3/numpy_zeros_and_ones.py
+++ python3/numpy_zeros_and_ones.py
@@ -1,8 +1,7 @@
 import numpy
-rows, columns, number_of_nested_arrays = map(int, input().split())
+input_parameters = tuple(map(int, input().split()))
 
-array_of_zeros = numpy.zeros((rows, columns, number_of_nested_arrays), dtype=int)
-array_of_ones = numpy.ones((rows, columns, number_of_nested_arrays), dtype=int)
+array_of_zeros = numpy.zeros(input_parameters, int)
+array_of_ones = numpy.ones(input_parameters, int)
 
-print(array_of_zeros)
-print(array_of_ones)
+print(array_of_zeros, array_of_ones, sep='\n')
