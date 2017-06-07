2de801e39a1f124891fcbfa5f147870954fc60f9
smhaggerty <shanehaggerty@umail.ucsb.edu>
2017-06-07 00:32:39 -0700

    moved challenges from Itertools subdomain to itertools directory

--- python3/combinations_with_replacement.py
+++
@@ -1,3 +0,0 @@
-from itertools import combinations_with_replacement as cwr
-s, k = input().split()
-print(*map(lambda x:''.join(x),cwr(sorted(s),int(k))),sep="\n")
--- python3/compress_the_string.py
+++
@@ -1,3 +0,0 @@
-from itertools import groupby
-s = input()
-print(*[(len(list(g)), k) for k, g in groupby(s, int)])
