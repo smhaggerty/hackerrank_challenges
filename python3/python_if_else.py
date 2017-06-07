51d2f8e3509b141ba8ccbc7d0243b1851d17bdb0
smhaggerty <shanehaggerty@umail.ucsb.edu>
2017-06-06 23:39:01 -0700

    moved to introduction directory

--- python3/python_if_else.py
+++
@@ -1,9 +0,0 @@
-if __name__ == '__main__':
-    n = int(input())
-    if n%2 != 0:
-        print("Weird")
-    else:
-        if 6 <= n and n <= 20:
-            print("Weird")
-        else:
-            print("Not Weird")
