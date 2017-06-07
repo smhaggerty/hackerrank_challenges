bbde0a0cef54c894affb0540db422f20aaf7638c
smhaggerty <shanehaggerty@umail.ucsb.edu>
2017-06-07 00:27:42 -0700

    moved challenges from Regex and Parsing subdomain to regex_and_parsing directory

--- python3/matrix_script.py
+++
@@ -1,8 +0,0 @@
-import re
-column_height, row_length = input().split()
-rows = [input() for _ in range(int(column_height))]
-string = str()
-for i in range(int(row_length)):
-    for j in range(int(column_height)):
-        string += rows[j][i]
-print(re.sub(r'(?<=\w)\W+(?=\w)', ' ',string))
