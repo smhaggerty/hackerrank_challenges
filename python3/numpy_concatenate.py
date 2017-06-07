b47b3408155a452dfe486fe0eb68c9270b912410
smhaggerty <shanehaggerty@umail.ucsb.edu>
2017-06-06 23:58:57 -0700

    moved to numpy directory

--- python3/numpy_concatenate.py
+++
@@ -1,7 +0,0 @@
-import numpy
-n, m, p = map(int, input().split())
-
-array_n = numpy.array([list(map(int,input().split())) for i in range(n)])
-array_p = numpy.array([list(map(int,input().split())) for i in range(m)])
-
-print(numpy.concatenate((array_n, array_p)))
