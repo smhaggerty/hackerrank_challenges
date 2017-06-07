d3e7949468857f635a340fae95c96b2a421baa67
smhaggerty <shanehaggerty@umail.ucsb.edu>
2017-06-06 23:59:14 -0700

    moved to numpy directory

--- python3/numpy_dot_and_cross.py
+++
@@ -1,6 +0,0 @@
-import numpy
-n = int(input())
-array_a = [list(map(int, input().split())) for i in range(n)]
-array_b = [list(map(int, input().split())) for i in range(n)]
-
-print(numpy.dot(array_a, array_b))
