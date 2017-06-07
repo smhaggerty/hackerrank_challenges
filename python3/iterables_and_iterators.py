84a4f1645af423f048987a0d07e8110a59424c59
smhaggerty <shanehaggerty@umail.ucsb.edu>
2017-06-07 00:39:11 -0700

    moved challenges from Itertools subdomain to itertools directory

--- python3/iterables_and_iterators.py
+++
@@ -1,4 +0,0 @@
-import itertools
-n, elements, k = float(input()), input().split(), int(input())
-num_combos_with_a = len([combo for combo in itertools.combinations(elements,k) if 'a' in combo])
-print(num_combos_with_a / len(list(itertools.combinations(elements,k))))
