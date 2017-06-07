58e90b87292cc36264b5db2739eb44b1975e3902
smhaggerty <shanehaggerty@umail.ucsb.edu>
2017-06-06 23:58:40 -0700

    moved to numpy directory

--- python3/numpy_array_mathematics.py
+++
@@ -1,9 +0,0 @@
-import numpy
-n, _ = map(int, input().split())
-
-array_a = [list(map(int, input().split())) for i in range(n)]
-array_b = [list(map(int, input().split())) for i in range(n)]
-methods = [numpy.add, numpy.subtract, numpy.multiply, numpy.divide, numpy.mod, numpy.power]
-
-for method in methods:
-    print(method(array_a, array_b).astype(int))
