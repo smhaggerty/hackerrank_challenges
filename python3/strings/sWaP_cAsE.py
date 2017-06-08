da86f51623661139179bd3d79dbbd5beaa331545
smhaggerty <shanehaggerty@umail.ucsb.edu>
2017-06-08 13:35:56 -0700

    added function call for solution

--- python3/strings/sWaP_cAsE.py
+++ python3/strings/sWaP_cAsE.py
@@ -1,3 +1,8 @@
 def swap_case(s):
     swaped_s = [char.upper() if char.islower() else char.lower() for char in s]
     return ''.join(swaped_s)
+
+if __name__ == '__main__':
+    s = input()
+    result = swap_case(s)
+    print(result)
