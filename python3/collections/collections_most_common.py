ca474e1fdea18e5c050322096ef262d0eea3e2d5
Shane Haggerty <shanehaggerty@Shanes-MacBook-Pro.local>
2017-06-20 22:12:53 -0700

    added better solution for 'Most Common' challenge

--- python3/collections/collections_most_common.py
+++ python3/collections/collections_most_common.py
@@ -1,16 +1,9 @@
 from collections import OrderedDict, Counter
 
 class OrderedCounter(Counter, OrderedDict):
-
-    def __repr__(self):
-        return '%s(%r)' % (self.__class__.__name__, OrderedDict(self))
-
-    def __reduce__(self):
-        return self.__class__, (OrderedDict(self),)
+     pass
 
 string = input()
-
-
 
 #find the top three,
 top_three = OrderedCounter(sorted(string)).most_common(3)
