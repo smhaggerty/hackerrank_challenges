60bca0294b34069d3ee0bdeff53705f90a2aa8ca
Shane Haggerty <shanehaggerty@umail.ucsb.edu>
2017-11-08 12:37:01 -0800

    added solutions

---
+++ ruby/ruby-tutorial/arrays_and_hashes/hash_each.rb
@@ -0,0 +1,7 @@
+def iter_hash(hash)
+    # your code here
+    hash.each {|key, val|
+      puts key
+      puts val
+    }
+end
--- ruby/ruby-tutorial/enumerables/collect.rb
+++ ruby/ruby-tutorial/enumerables/collect.rb
@@ -1,3 +1,4 @@
 def rot13(secret_messages)
     # your code here
-end
+    secret_messages.map {|i| i.tr('A-Za-z', 'N-ZA-Mn-za-m')}
+end  
